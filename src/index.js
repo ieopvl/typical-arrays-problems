9fdac391daa5f5e41b34e1f398128560ac061752
Pavel Ivchenko <pavel.ivchenko.js@mail.ru>
2020-09-29 19:39:43 +0300

    feat: implement max min avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+const sorted = (array ) => array.sort((a, b) => a - b);
+const validate = (array) =>  ( !array || array.length === 0 );
 
 exports.min = function min (array) {
-  return 0;
+  return !validate(array) ? sorted(array)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !validate(array) ? sorted(array)[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !validate(array) ? array.reduce( (acc, cur) => acc + cur, 0 ) / array.length : 0;
 }
